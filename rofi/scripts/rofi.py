11cc67883a327e11cf327cec0d422d1dd84160d4
Michael Benford <michael@michaelbenford.net>
2021-04-26 23:02:50 -0300

    rofi: rename rofi.py as rofipie.py

--- rofi/scripts/rofi.py
+++ rofi/scripts/rofi.py
@@ -1,3 +1,4 @@
+import sys
 from os import getenv
 
 
@@ -8,6 +9,11 @@ class Rofi:
         info = getenv("ROFI_INFO", "")
         self.__info = [] if info == "" else info.split(";")
 
+    def handle_startup(self, generate, process):
+        if len(sys.argv) == 1:
+            generate(self)
+        else:
+            process(self)
 
     def row(self, text, meta=None, info=None):
         opts = []
@@ -22,33 +28,30 @@ class Rofi:
 
         opts = "\x1f".join(opts)
         print(f"{text}\0{opts}")
-
+        return self
 
     def info(self, index=None):
         return self.__info if index is None else self.__info[index]
 
-
     def retv(self):
         return None if self.__retv is None else int(self.__retv)
 
-
     def prompt(self, text):
         self.__set_option("prompt", text)
-
+        return self
 
     def message(self, text):
         self.__set_option("message", text)
-
+        return self
 
     def markup_rows(self, enabled):
         self.__set_option("markup-rows", str(enabled).lower())
-
+        return self
 
     def custom_entry(self, enabled):
         self.__set_option("no-custom", str(enabled).lower())
+        return self
 
-
-    def __set_option(self, name, value):
+    @staticmethod
+    def __set_option(name, value):
         print(f"\0{name}\x1f{value}")
-
-
