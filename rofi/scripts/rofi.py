eac086199c60647a5bdd0422df8c311886c8ae82
Michael Benford <michael@michaelbenford.net>
2021-05-29 23:22:48 -0300

    rofi: update custom scripts

--- rofi/scripts/rofi.py
+++ rofi/scripts/rofi.py
@@ -15,7 +15,7 @@ class Rofi:
         else:
             process(self)
 
-    def row(self, text, meta=None, info=None):
+    def row(self, text, meta=None, info=None, selectable=True):
         opts = []
         opt_fmt = "{name}\x1f{value}"
 
@@ -26,12 +26,17 @@ class Rofi:
             opts.append(opt_fmt.format(name="info",
                                        value=";".join([*self.__info, str(info)])))
 
+        if not selectable:
+            opts.append(opt_fmt.format(name="nonselectable", value="true"))
+
         opts = "\x1f".join(opts)
         print(f"{text}\0{opts}")
         return self
 
     def info(self, index=None):
-        return self.__info if index is None else self.__info[index]
+        if index is None:
+            return self.__info
+        return self.__info[index] if index < len(self.__info) else None
 
     def retv(self):
         return None if self.__retv is None else int(self.__retv)
