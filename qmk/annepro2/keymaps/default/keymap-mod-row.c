a4cb077acdba34bb6870af04d28656d327382a9a
Michael Benford <michael@michaelbenford.net>
2022-07-03 22:14:13 -0300

    qmk: update config

--- qmk/annepro2/keymaps/default/keymap-mod-row.c
+++ qmk/annepro2/keymaps/default/keymap-mod-row.c
@@ -9,25 +9,37 @@ enum anne_pro_layers {
 	FN2,
 };
 
+// Left-hand home row mods
+#define HOME_A LGUI_T(KC_A)
+#define HOME_S LALT_T(KC_S)
+#define HOME_D LSFT_T(KC_D)
+#define HOME_F LCTL_T(KC_F)
+
+// Right-hand home row mods
+#define HOME_J RCTL_T(KC_J)
+#define HOME_K RSFT_T(KC_K)
+#define HOME_L LALT_T(KC_L)
+#define HOME_SCLN RALT_T(KC_SCLN)
+
 const uint16_t keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 	[DEF] = KEYMAP(
     KC_GRV, KC_1, KC_2, KC_3, KC_4, KC_5, KC_6, KC_7, KC_8, KC_9, KC_0, KC_MINS, KC_EQL, KC_BSPC,
     KC_TAB, KC_Q, KC_W, KC_E, KC_R, KC_T, KC_Y, KC_U, KC_I, KC_O, KC_P, KC_LBRC, KC_RBRC, KC_BSLS,
-    MO(FN1), KC_A, KC_S, KC_D, KC_F, KC_G, KC_H, KC_J, KC_K, KC_L, KC_SCLN, KC_QUOT, KC_ENT,
+    MO(FN1), HOME_A, HOME_S, HOME_D, HOME_F, KC_G, KC_H, HOME_J, HOME_K, HOME_L, HOME_SCLN, KC_QUOT, KC_ENT,
     KC_LSFT, KC_Z, KC_X, KC_C, KC_V, KC_B, KC_N, KC_M, KC_COMM, KC_DOT, KC_SLSH, KC_RSFT,
-    KC_LCTL, KC_LALT, KC_LGUI, CTL_T(KC_SPC), OSM(MOD_RALT), KC_RCTL, MO(FN1), MO(FN2)
+    KC_LCTL, KC_LALT, KC_LGUI, KC_SPC, OSM(MOD_RALT), KC_RCTL, MO(FN1), MO(FN2)
 	),
 
 	[FN1] = KEYMAP(
-    KC_ESC, KC_F1, KC_F2, KC_F3, KC_F4, KC_F5, KC_F6, KC_F7, KC_F8, KC_F9, KC_F10, KC_F11, KC_F12, KC_DEL,
-    _______, _______, _______, KC_ESC, _______, _______, _______, _______, KC_HOME, KC_END, _______, _______, _______, KC_CAPS,
-    _______, KC_LALT, KC_LSFT, KC_LCTL, _______, _______, KC_LEFT, KC_DOWN, KC_UP, KC_RIGHT, KC_BSPC, KC_DEL, KC_PSCR,
+    _______, KC_F1, KC_F2, KC_F3, KC_F4, KC_F5, KC_F6, KC_F7, KC_F8, KC_F9, KC_F10, KC_F11, KC_F12, KC_DEL,
+    _______, _______, _______, KC_ESC, _______, _______, _______, KC_PERC, KC_HOME, KC_END, _______, _______, _______, KC_CAPS,
+    _______, KC_LGUI, KC_LALT, KC_LSFT, KC_LCTL, _______, KC_LEFT, KC_DOWN, KC_UP, KC_RIGHT, KC_BSPC, KC_DEL, KC_PSCR,
     _______, _______, S(KC_DEL), C(KC_INS), S(KC_INS), KC_MEDIA_PLAY_PAUSE, KC_MPRV, KC_MNXT, KC_VOLD, KC_VOLU, KC_MUTE, _______,
     _______, _______, _______, KC_ENT, _______, _______, _______, _______
 	),
 
 	[FN2] = KEYMAP(
-    _______, KC_AP2_BT1, KC_AP2_BT2, KC_AP2_BT3, KC_AP2_BT4, _______, _______, _______, _______, _______, _______, KC_AP_LED_SPEED, KC_AP_LED_NEXT_INTENSITY, KC_AP_LED_ON,
+    _______, KC_AP2_BT1, KC_AP2_BT2, KC_AP2_BT3, KC_AP2_BT4, _______, _______, _______, _______, _______, _______, _______, KC_AP_LED_NEXT_INTENSITY, KC_AP_LED_ON,
     _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, KC_AP_LED_OFF,
     _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,
     _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,
