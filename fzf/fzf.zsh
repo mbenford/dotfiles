77bc3b95c17f543f7bdb09aca800b493501bb635
Michael Benford <michael@michaelbenford.net>
2023-12-13 21:26:08 -0300

    fzf: add config

---
+++ fzf/fzf.zsh
@@ -0,0 +1,12 @@
+export FZF_DEFAULT_COMMAND="rg --files --hidden"
+export FZF_DEFAULT_OPTS='
+  --ansi
+	--no-info
+	--height=~100%
+	--reverse
+	--prompt=" "
+	--pointer="▍"
+	--marker="✗"
+	--color=bg+:#363a4f,bg:#24273a,spinner:#f4dbd6,hl:#ed8796
+	--color=fg:#cad3f5,header:#ed8796,info:#c6a0f6,pointer:#f4dbd6
+	--color=marker:#f4dbd6,fg+:#cad3f5,prompt:#c6a0f6,hl+:#ed8796'
